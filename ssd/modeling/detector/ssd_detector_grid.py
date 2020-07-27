7c2d246d2107e3ece7290b811e77050723b7cfbb
liangshb <liangshb@outlook.com>
2020-07-27 10:01:22 +0800

    data augmentation with grid

---
+++ ssd/modeling/detector/ssd_detector_grid.py
@@ -0,0 +1,22 @@
+from torch import nn
+
+from ssd.modeling.backbone import build_backbone
+from ssd.modeling.box_head import build_box_head
+from ssd.data.transforms.grid import GridMask
+
+
+class SSDDetector(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        self.grid = GridMask(True, True, 1, 0, 0.5, 1, 0.7)
+        self.backbone = build_backbone(cfg)
+        self.box_head = build_box_head(cfg)
+
+    def forward(self, images, targets=None):
+        images = self.grid(images)
+        features = self.backbone(images)
+        detections, detector_losses = self.box_head(features, targets)
+        if self.training:
+            return detector_losses
+        return detections
